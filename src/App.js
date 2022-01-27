7678a6e072267e621dd61023ace3bbd60c833290
VladimirVarelaH <vladimir.varelah@gmail.com>
2022-01-26 23:18:45 -0300

    Se renombro la carpeta Section5

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Seccion1 } from "./Section/Section1/seccion1";
 import { Seccion2 } from "./Section/Section2/seccion2";
 import { Seccion3 } from "./Section/Section3/seccion3";
 import { Seccion4 } from "./Section/Section4/seccion4";
-import { Seccion5 } from "./components/seccion5";
+import { Seccion5 } from "./Section/Section5/index.jsx";
 import Footer from "./Footer/footer";
 
 
